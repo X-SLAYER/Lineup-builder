0d2d12897748f15837bbe8234d54f38d67683c0b
Iheb <slayertn1999@gmail.com>
2021-02-27 19:36:39 +0100

    add mailot widget

---
+++ lib/widgets/mailot.dart
@@ -0,0 +1,488 @@
+import 'package:flutter/material.dart';
+
+class RPSCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(size.width * 0.3702771, size.height * 0.02122642);
+    path_0.lineTo(size.width * 0.3929471, 0);
+    path_0.arcToPoint(Offset(size.width * 0.5340050, size.height * 0.004716981),
+        radius:
+            Radius.elliptical(size.width * 1.407935, size.height * 1.318278),
+        rotation: 0,
+        largeArc: false,
+        clockwise: false);
+    path_0.cubicTo(
+        size.width * 0.5340050,
+        size.height * 0.004716981,
+        size.width * 0.5810252,
+        size.height * 0.001573113,
+        size.width * 0.5944584,
+        0);
+    path_0.lineTo(size.width * 0.6045340, 0);
+    path_0.cubicTo(
+        size.width * 0.6045340,
+        0,
+        size.width * 0.6213275,
+        size.height * 0.01336557,
+        size.width * 0.6297229,
+        size.height * 0.01886792);
+    path_0.cubicTo(
+        size.width * 0.6297229,
+        size.height * 0.01886792,
+        size.width * 0.8219975,
+        size.height * 0.08647877,
+        size.width * 0.8413098,
+        size.height * 0.09433962);
+    path_0.cubicTo(
+        size.width * 0.8413098,
+        size.height * 0.09433962,
+        size.width * 0.8463476,
+        size.height * 0.09119575,
+        size.width * 0.8639798,
+        size.height * 0.1273585);
+    path_0.cubicTo(
+        size.width * 0.8639798,
+        size.height * 0.1273585,
+        size.width * 0.8807733,
+        size.height * 0.1548750,
+        size.width * 0.9068010,
+        size.height * 0.2146226);
+    path_0.cubicTo(
+        size.width * 0.9328287,
+        size.height * 0.2743703,
+        size.width * 0.9706121,
+        size.height * 0.3632075,
+        size.width * 0.9748111,
+        size.height * 0.3750000);
+    path_0.cubicTo(
+        size.width * 0.9748111,
+        size.height * 0.3750000,
+        size.width * 0.9958010,
+        size.height * 0.4213844,
+        size.width * 0.9974811,
+        size.height * 0.4339623);
+    path_0.cubicTo(
+        size.width * 0.9974811,
+        size.height * 0.4339623,
+        size.width * 1.005877,
+        size.height * 0.4378939,
+        size.width * 0.9949622,
+        size.height * 0.4457547);
+    path_0.cubicTo(
+        size.width * 0.9949622,
+        size.height * 0.4457547,
+        size.width * 0.9311511,
+        size.height * 0.4795590,
+        size.width * 0.9118388,
+        size.height * 0.4882075);
+    path_0.cubicTo(
+        size.width * 0.9118388,
+        size.height * 0.4882075,
+        size.width * 0.8513854,
+        size.height * 0.5086486,
+        size.width * 0.8261965,
+        size.height * 0.5117925);
+    path_0.cubicTo(
+        size.width * 0.8261965,
+        size.height * 0.5117925,
+        size.width * 0.8161209,
+        size.height * 0.5157241,
+        size.width * 0.8085642,
+        size.height * 0.5141509);
+    path_0.cubicTo(
+        size.width * 0.8010076,
+        size.height * 0.5125778,
+        size.width * 0.7968086,
+        size.height * 0.4882075,
+        size.width * 0.7858942,
+        size.height * 0.4740566);
+    path_0.lineTo(size.width * 0.7632242, size.height * 0.4245283);
+    path_0.lineTo(size.width * 0.7657431, size.height * 0.9551887);
+    path_0.cubicTo(
+        size.width * 0.7657431,
+        size.height * 0.9551887,
+        size.width * 0.7758186,
+        size.height * 0.9677665,
+        size.width * 0.7556675,
+        size.height * 0.9811321);
+    path_0.cubicTo(
+        size.width * 0.7556675,
+        size.height * 0.9811321,
+        size.width * 0.5373627,
+        size.height * 1.016509,
+        size.width * 0.2921914,
+        size.height * 0.9882075);
+    path_0.cubicTo(
+        size.width * 0.2921914,
+        size.height * 0.9882075,
+        size.width * 0.2493703,
+        size.height * 0.9819175,
+        size.width * 0.2317380,
+        size.height * 0.9693396);
+    path_0.lineTo(size.width * 0.2392947, size.height * 0.4245283);
+    path_0.cubicTo(
+        size.width * 0.2392947,
+        size.height * 0.4245283,
+        size.width * 0.2359370,
+        size.height * 0.4261014,
+        size.width * 0.2292191,
+        size.height * 0.4410377);
+    path_0.cubicTo(
+        size.width * 0.2292191,
+        size.height * 0.4410377,
+        size.width * 0.2006725,
+        size.height * 0.5031439,
+        size.width * 0.2015113,
+        size.height * 0.5070755);
+    path_0.cubicTo(
+        size.width * 0.2015113,
+        size.height * 0.5070755,
+        size.width * 0.1905970,
+        size.height * 0.5165094,
+        size.width * 0.1889169,
+        size.height * 0.5165094);
+    path_0.cubicTo(
+        size.width * 0.1872368,
+        size.height * 0.5165094,
+        size.width * 0.1335013,
+        size.height * 0.5039316,
+        size.width * 0.1007557,
+        size.height * 0.4905660);
+    path_0.cubicTo(
+        size.width * 0.1007557,
+        size.height * 0.4905660,
+        size.width * 0.02686902,
+        size.height * 0.4575472,
+        size.width * 0.01511335,
+        size.height * 0.4528302);
+    path_0.cubicTo(size.width * 0.01511335, size.height * 0.4528302, 0,
+        size.height * 0.4496863, 0, size.height * 0.4363208);
+    path_0.cubicTo(
+        0,
+        size.height * 0.4363208,
+        size.width * 0.07640554,
+        size.height * 0.2437099,
+        size.width * 0.08816121,
+        size.height * 0.2216981);
+    path_0.cubicTo(
+        size.width * 0.08816121,
+        size.height * 0.2216981,
+        size.width * 0.1351814,
+        size.height * 0.1202830,
+        size.width * 0.1435768,
+        size.height * 0.1084906);
+    path_0.cubicTo(
+        size.width * 0.1435768,
+        size.height * 0.1084906,
+        size.width * 0.1494534,
+        size.height * 0.09984198,
+        size.width * 0.1738035,
+        size.height * 0.08962264);
+    path_0.lineTo(size.width * 0.2896725, size.height * 0.04716981);
+    path_0.lineTo(size.width * 0.3501259, size.height * 0.02594340);
+    path_0.close();
+
+    Paint paint_0_fill = Paint()..style = PaintingStyle.fill;
+    paint_0_fill.color = Color(0xffb81313).withOpacity(1.0);
+    canvas.drawPath(path_0, paint_0_fill);
+
+    Path path_1 = Path();
+    path_1.moveTo(size.width * 0.3677582, size.height * 0.02122642);
+    path_1.cubicTo(
+        size.width * 0.3677582,
+        size.height * 0.02122642,
+        size.width * 0.3895894,
+        size.height * 0.002358491,
+        size.width * 0.3954660,
+        0);
+    path_1.cubicTo(
+        size.width * 0.4013426,
+        size.height * -0.002358491,
+        size.width * 0.4458438,
+        size.height * 0.005502358,
+        size.width * 0.5138539,
+        size.height * 0.004716981);
+    path_1.cubicTo(
+        size.width * 0.5138539,
+        size.height * 0.004716981,
+        size.width * 0.5936196,
+        size.height * 0.003143868,
+        size.width * 0.5994962,
+        0);
+    path_1.cubicTo(
+        size.width * 0.5994962,
+        0,
+        size.width * 0.6154484,
+        size.height * 0.006290094,
+        size.width * 0.6221662,
+        size.height * 0.01415094);
+    path_1.cubicTo(
+        size.width * 0.6221662,
+        size.height * 0.01415094,
+        size.width * 0.6305617,
+        size.height * 0.03380425,
+        size.width * 0.6171285,
+        size.height * 0.05660377);
+    path_1.cubicTo(
+        size.width * 0.6171285,
+        size.height * 0.05660377,
+        size.width * 0.5734685,
+        size.height * 0.1367925,
+        size.width * 0.5214106,
+        size.height * 0.1745283);
+    path_1.lineTo(size.width * 0.5037783, size.height * 0.1910377);
+    path_1.lineTo(size.width * 0.4962217, size.height * 0.1910377);
+    path_1.cubicTo(
+        size.width * 0.4962217,
+        size.height * 0.1910377,
+        size.width * 0.4534005,
+        size.height * 0.1611627,
+        size.width * 0.4307305,
+        size.height * 0.1344340);
+    path_1.cubicTo(
+        size.width * 0.4307305,
+        size.height * 0.1344340,
+        size.width * 0.4021839,
+        size.height * 0.1022005,
+        size.width * 0.3904282,
+        size.height * 0.07783019);
+    path_1.cubicTo(
+        size.width * 0.3904282,
+        size.height * 0.07783019,
+        size.width * 0.3778338,
+        size.height * 0.04874292,
+        size.width * 0.3753149,
+        size.height * 0.04009434);
+    path_1.close();
+
+    Paint paint_1_fill = Paint()..style = PaintingStyle.fill;
+    paint_1_fill.color = Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_1, paint_1_fill);
+
+    Path path_2 = Path();
+    path_2.moveTo(size.width * 0.4030227, size.height * 0.02830189);
+    path_2.cubicTo(
+        size.width * 0.4030227,
+        size.height * 0.02830189,
+        size.width * 0.5785063,
+        size.height * 0.03223349,
+        size.width * 0.5944584,
+        size.height * 0.02830189);
+    path_2.cubicTo(
+        size.width * 0.5944584,
+        size.height * 0.02830189,
+        size.width * 0.6011763,
+        size.height * 0.03459198,
+        size.width * 0.5869018,
+        size.height * 0.05896226);
+    path_2.cubicTo(
+        size.width * 0.5869018,
+        size.height * 0.05896226,
+        size.width * 0.5575139,
+        size.height * 0.1124222,
+        size.width * 0.5289673,
+        size.height * 0.1391509);
+    path_2.lineTo(size.width * 0.5012594, size.height * 0.1650943);
+    path_2.cubicTo(
+        size.width * 0.5012594,
+        size.height * 0.1650943,
+        size.width * 0.4256927,
+        size.height * 0.09984198,
+        size.width * 0.4156171,
+        size.height * 0.07075472);
+    path_2.cubicTo(
+        size.width * 0.4156171,
+        size.height * 0.07075472,
+        size.width * 0.4055416,
+        size.height * 0.05503066,
+        size.width * 0.4030227,
+        size.height * 0.04245283);
+    path_2.cubicTo(
+        size.width * 0.4005038,
+        size.height * 0.02987500,
+        size.width * 0.4005038,
+        size.height * 0.02830189,
+        size.width * 0.4005038,
+        size.height * 0.02830189);
+
+    Paint paint_2_fill = Paint()..style = PaintingStyle.fill;
+    paint_2_fill.color = Color(0xffb81313).withOpacity(1.0);
+    canvas.drawPath(path_2, paint_2_fill);
+
+    Path path_3 = Path();
+    path_3.moveTo(size.width * 0.007556675, size.height * 0.4150943);
+    path_3.cubicTo(
+        size.width * 0.007556675,
+        size.height * 0.4150943,
+        size.width * 0.1041134,
+        size.height * 0.4685542,
+        size.width * 0.1435768,
+        size.height * 0.4764151);
+    path_3.cubicTo(
+        size.width * 0.1435768,
+        size.height * 0.4764151,
+        size.width * 0.1998312,
+        size.height * 0.4913514,
+        size.width * 0.2040302,
+        size.height * 0.4905660);
+    path_3.cubicTo(
+        size.width * 0.2082292,
+        size.height * 0.4897807,
+        size.width * 0.1992116,
+        size.height * 0.5107901,
+        size.width * 0.1914358,
+        size.height * 0.5141509);
+    path_3.cubicTo(
+        size.width * 0.1805214,
+        size.height * 0.5188679,
+        size.width * 0.1880781,
+        size.height * 0.5149363,
+        size.width * 0.1259446,
+        size.height * 0.5023585);
+    path_3.cubicTo(
+        size.width * 0.1259446,
+        size.height * 0.5023585,
+        size.width * 0.05793451,
+        size.height * 0.4748420,
+        size.width * 0.03778338,
+        size.height * 0.4646226);
+    path_3.cubicTo(
+        size.width * 0.03778338,
+        size.height * 0.4646226,
+        size.width * 0.004198992,
+        size.height * 0.4481132,
+        0,
+        size.height * 0.4410377);
+    path_3.arcToPoint(Offset(0, size.height * 0.4292453),
+        radius: Radius.elliptical(
+            size.width * 0.05546096, size.height * 0.05192925),
+        rotation: 0,
+        largeArc: false,
+        clockwise: true);
+    path_3.cubicTo(
+        size.width * 0.0008387909,
+        size.height * 0.4237429,
+        size.width * 0.007556675,
+        size.height * 0.4150943,
+        size.width * 0.007556675,
+        size.height * 0.4150943);
+    path_3.close();
+
+    Paint paint_3_fill = Paint()..style = PaintingStyle.fill;
+    paint_3_fill.color = Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_3, paint_3_fill);
+
+    Path path_4 = Path();
+    path_4.moveTo(size.width * 0.9920076, size.height * 0.4150943);
+    path_4.cubicTo(
+        size.width * 0.9920076,
+        size.height * 0.4150943,
+        size.width * 0.8949320,
+        size.height * 0.4687476,
+        size.width * 0.8552569,
+        size.height * 0.4766509);
+    path_4.cubicTo(
+        size.width * 0.8552569,
+        size.height * 0.4766509,
+        size.width * 0.7987003,
+        size.height * 0.4916415,
+        size.width * 0.7944786,
+        size.height * 0.4908538);
+    path_4.cubicTo(
+        size.width * 0.7902569,
+        size.height * 0.4900660,
+        size.width * 0.7993249,
+        size.height * 0.5111509,
+        size.width * 0.8071411,
+        size.height * 0.5145236);
+    path_4.cubicTo(
+        size.width * 0.8181159,
+        size.height * 0.5192571,
+        size.width * 0.8105189,
+        size.height * 0.5153137,
+        size.width * 0.8729849,
+        size.height * 0.5026887);
+    path_4.cubicTo(
+        size.width * 0.8729849,
+        size.height * 0.5026887,
+        size.width * 0.9413602,
+        size.height * 0.4750731,
+        size.width * 0.9616171,
+        size.height * 0.4648160);
+    path_4.cubicTo(
+        size.width * 0.9616171,
+        size.height * 0.4648160,
+        size.width * 0.9953703,
+        size.height * 0.4482453,
+        size.width * 0.9996045,
+        size.height * 0.4411439);
+    path_4.arcToPoint(Offset(size.width * 0.9996045, size.height * 0.4293090),
+        radius: Radius.elliptical(
+            size.width * 0.05557935, size.height * 0.05204009),
+        rotation: 0,
+        largeArc: false,
+        clockwise: false);
+    path_4.cubicTo(
+        size.width * 0.9987607,
+        size.height * 0.4237736,
+        size.width * 0.9920076,
+        size.height * 0.4150943,
+        size.width * 0.9920076,
+        size.height * 0.4150943);
+    path_4.close();
+
+    Paint paint_4_fill = Paint()..style = PaintingStyle.fill;
+    paint_4_fill.color = Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_4, paint_4_fill);
+
+    Path path_5 = Path();
+    path_5.moveTo(size.width * 0.8438287, size.height * 0.09905660);
+    path_5.cubicTo(
+        size.width * 0.8438287,
+        size.height * 0.09905660,
+        size.width * 0.7663728,
+        size.height * 0.3248821,
+        size.width * 0.7657431,
+        size.height * 0.3844340);
+    path_5.cubicTo(
+        size.width * 0.7644836,
+        size.height * 0.4038915,
+        size.width * 0.8255668,
+        size.height * 0.1438679,
+        size.width * 0.8488665,
+        size.height * 0.1037736);
+    path_5.close();
+
+    Paint paint_5_fill = Paint()..style = PaintingStyle.fill;
+    paint_5_fill.color = Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_5, paint_5_fill);
+
+    Path path_6 = Path();
+    path_6.moveTo(size.width * 0.1561713, size.height * 0.09905660);
+    path_6.cubicTo(
+        size.width * 0.1561713,
+        size.height * 0.09905660,
+        size.width * 0.2336272,
+        size.height * 0.3240684,
+        size.width * 0.2342569,
+        size.height * 0.3834057);
+    path_6.cubicTo(
+        size.width * 0.2355164,
+        size.height * 0.4027925,
+        size.width * 0.1744332,
+        size.height * 0.1437075,
+        size.width * 0.1511335,
+        size.height * 0.1037571);
+    path_6.close();
+
+    Paint paint_6_fill = Paint()..style = PaintingStyle.fill;
+    paint_6_fill.color = Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_6, paint_6_fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:lineup_builder/constants/text_styles.dart';
 import 'package:lineup_builder/models/player_positions.dart';
 
+import 'mailot.dart';
+
 class Player extends StatefulWidget {
   final PlayerPosition coordinates;
   final String position;
@@ -14,57 +16,62 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
-  TextEditingController _controller = TextEditingController(text: 'Player');
-  FocusNode _focusNode = FocusNode();
+  GlobalKey _key = GlobalKey();
+  double top, left;
+  double xOff, yOff;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+    top = widget.coordinates.offset.dy;
+    left = widget.coordinates.offset.dx;
+    super.initState();
+  }
+
+  void _getRenderOffsets() {
+    final RenderBox renderBoxWidget = _key.currentContext.findRenderObject();
+    final offset = renderBoxWidget.localToGlobal(Offset.zero);
+
+    yOff = offset.dy - this.top;
+    xOff = offset.dx - this.left;
+  }
+
+  void _afterLayout(_) {
+    _getRenderOffsets();
+  }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedPositioned(
+      key: _key,
       duration: Duration(milliseconds: 0),
-      top: widget.coordinates.offset.dy,
-      left: widget.coordinates.offset.dx,
+      top: top,
+      left: left,
       child: Draggable(
-        child: _playerSpot(),
-        feedback: _playerSpot(),
-        childWhenDragging: Container(),
-        onDraggableCanceled: (_, Offset offset) {
-          print(offset);
-          setState(() {
-            widget.coordinates.initCords(offset);
-          });
-        },
-      ),
+          child: _playerSpot(),
+          feedback: _playerSpot(),
+          childWhenDragging: Container(),
+          onDragEnd: (drag) {
+            setState(() {
+              top = drag.offset.dy - yOff;
+              left = drag.offset.dx - xOff;
+            });
+          }),
     );
   }
 
   Widget _playerSpot() => Material(
-        color: Colors.transparent,
-        child: Column(
-          children: [
-            Container(
-              width: 45.0,
-              height: 45.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: widget.color,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white.withOpacity(0.5),
-                    spreadRadius: 4,
-                    blurRadius: 7,
-                    offset: Offset(1, 1),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Text(widget.position),
-              ),
-            ),
-            SizedBox(height: 8.0),
-            _playerName()
-          ],
-        ),
-      );
+      color: Colors.transparent,
+      child: Column(
+        children: [
+          CustomPaint(
+            size: Size(50, (50 * 1.0680100755667505).toDouble()),
+            painter: RPSCustomPainter(),
+          ),
+          SizedBox(height: 8.0),
+          _playerName(),
+        ],
+      ));
 
   Widget _playerName() => Text(
         "Player",
@@ -72,3 +79,29 @@ class _PlayerState extends State<Player> {
         style: playerName(),
       );
 }
+
+// Column(
+//           children: [
+//             Container(
+//               width: 45.0,
+//               height: 45.0,
+//               decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 color: widget.color,
+//                 boxShadow: [
+//                   BoxShadow(
+//                     color: Colors.white.withOpacity(0.5),
+//                     spreadRadius: 4,
+//                     blurRadius: 7,
+//                     offset: Offset(1, 1),
+//                   ),
+//                 ],
+//               ),
+//               child: Center(
+//                 child: Text(widget.position),
+//               ),
+//             ),
+//             SizedBox(height: 8.0),
+//             _playerName()
+//           ],
+//         ),
